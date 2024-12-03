1bcccc2a98f2dfb660a3b230aa55e0f8b536ee0a
uJFalkez <matubaramarcos@gmail.com>
2024-12-03 11:12:55 -0300

    segunda entrega

--- simu.py
+++ simu.py
@@ -10,6 +10,7 @@ A_f = 50
 U_t = 6.7             # Coef. global (condução + convecção) do vidro
 U_s = 6.8             # Coef. global (condução + convecção) do PE Duplo
 T_H = 273+26
+T_2 = 273+20
 T_0 = 273+14
 
 T_inf = 273+10
@@ -19,26 +20,43 @@ cSB = 5.67e-8
 eps_s = 0.85
 eps_a = 0.883
 
-h_c = 15
+h_c  = 15
+h_12 = 3.4
+h_23 = 3.4
 
+rho_ar = 1.2        # kg/m³
+cp_ar  = 1004       # J/kgK
+m_ar   = 8.13e-2    # kg/s
+
+cp_nh3 = 2230       # J/kgK
+m_nh3  = 0.06       # kg/s
+
+Q_12 = A_f*h_12*(T_H - T_2)
+Q_21 = -Q_12
+Q_23 = A_f*h_12*(T_2-T_0)
+Q_32 = -Q_23
+
+Q_teto  = A_f*U_t*(T_H - T_inf)
+Q_solo  = A_f*U_s*(T_0 - T_solo)
+Q_rsolo = eps_s*Tau_s*A_f*cSB*(T_0**4-eps_a*T_solo**4)
 
 days = [x+20 for x in [-30,1,32,60,91,121,152,182,213,244,274,305,335,366]]
 
 df_abc = pd.DataFrame([
-[1233,0.142,0.057],
-[1230,0.142,0.058],
-[1214,0.144,0.060],
-[1185,0.156,0.071],
-[1135,0.180,0.097],
-[1104,0.196,0.121],
-[1088,0.205,0.134],
 [1085,0.207,0.136],
 [1107,0.201,0.122],
 [1151,0.177,0.092],
 [1192,0.160,0.073],
 [1120,0.149,0.063],
 [1233,0.142,0.057],
-[1230,0.142,0.058]],
+[1230,0.142,0.058],
+[1233,0.142,0.057],
+[1230,0.142,0.058],
+[1214,0.144,0.060],
+[1185,0.156,0.071],
+[1135,0.180,0.097],
+[1104,0.196,0.121],
+[1088,0.205,0.134]],
 columns=['a','b','c'], index=days)
 
 def interpolate(s,x,y):
@@ -60,7 +78,7 @@ def get_abc(dia):
     return a,b,c
 
 def beta_sol(dia):
-    return np.pi/2+np.sin(2*np.pi*(dia-offset_dia0)/(365))*0.4101524
+    return np.pi/2+np.cos(2*np.pi*(dia-offset_dia0)/365)*0.4101524/3
 
 def beta_dia(seg):
     seg = seg%86400
@@ -72,27 +90,144 @@ def beta(seg):
 
 def I(seg):
     a,b,c = get_abc(seg//86400)
-    beta_ = np.sin(beta(seg))
-    return (c+beta_)*a*(0 if beta_ == 0 else np.e**(-b/beta_))
+    beta_ = abs(np.sin(beta(seg)))
+    if beta_ < 10e-3: return 0
+    return (c+beta_)*a*np.e**(-b/beta_)
 
 def Q_rad(seg):
     return Tau_t*I(seg)*A_f
 
-#d = np.linspace(1,7*86400,10000)
-#Q = []
-
-#for d_ in d:
-    Q.append(Q_rad(d_))
+def m_nh3_corr(s0, corr_range):
+    T_min = 285.8
+    T_max = 302
+    T_ar = 283
     
-#plt.plot(d/86400,Q)
-#plt.show()
+    m_nh3_aj = []
+    for s in range(corr_range):
+        T0_ar = T_ar
+        T_ar = T0_ar + (T_max-T_min)*np.sin(np.pi*((s0+s)-5*60)/(14*60))
+        
+        m_nh3_aj.append(((Q_rad((s0+s)*60)-Q_teto-Q_solo-Q_rsolo)-m_ar*cp_ar*(T_ar-T0_ar))/cp_nh3)
+        
+    return m_nh3_aj
 
-Q_teto  = A_f*U_t*(T_H - T_inf)
-Q_solo  = A_f*U_s*(T_0 - T_solo)
-Q_rsolo = eps_s*Tau_s*A_f*cSB*(T_0**4-eps_a*T_solo**4)
+def Q_VC1_corr(s0, corr_range):
+    T_min = 285.8
+    T_max = 302
+    T_ar = 283
+    
+    Q_aj = []
+    for s in range(corr_range):
+        T0_ar = T_ar
+        T_ar = T0_ar + (T_max-T_min)*np.sin(np.pi*((s0+s)-5*60)/(14*60))
+        
+        Q_VC1 = Q_rad(s0+s)-Q_teto-Q_12 + m_ar*cp_ar*(T_ar-T0_ar)
+        
+        Q_aj.append(-Q_VC1)
+        
+    return Q_aj
 
-Q_VC = Q_rad(86400/2)-Q_teto-Q_solo-Q_rsolo
+def Q_VC2_corr(s0, corr_range):
+    T_min = 285.8
+    T_max = 302
+    T_ar = 283
+    
+    Q_aj = []
+    for s in range(corr_range):
+        T0_ar = T_ar
+        T_ar = T0_ar + (T_max-T_min)*np.sin(np.pi*((s0+s)-5*60)/(14*60))
+        
+        Q_VC2 = Q_21-Q_23 + m_ar*cp_ar*(T_ar-T0_ar)
+        
+        Q_aj.append(-Q_VC2)
+        
+    return Q_aj
+    
+def Q_VC3_corr(s0, corr_range):
+    T_min = 285.8
+    T_max = 302
+    T_ar = 283
+    
+    Q_aj = []
+    for s in range(corr_range):
+        T0_ar = T_ar
+        T_ar = T0_ar + (T_max-T_min)*np.sin(np.pi*((s0+s)-5*60)/(14*60))
+        
+        Q_VC3 = Q_32-Q_solo-Q_rsolo + m_ar*cp_ar*(T_ar-T0_ar) 
+        
+        Q_aj.append(-Q_VC3)
+        
+    return Q_aj    
 
-T = Q_VC/(A_f*15)+20
+def plot(type, di, df):
+    di = di*1440
+    df = df*1440
+    days = df-di
+    d = np.linspace(di,df,days)
+    Q_VC  = []
+    Q_VC1 = []
+    Q_VC2 = Q_12+Q_32
+    Q_VC3 = Q_23-Q_solo-Q_rsolo
 
-print(T)
+    for d_ in d:
+        Q_rad_ = Q_rad(d_)
+        Q_VC.append(Q_rad_-Q_teto-Q_solo-Q_rsolo)
+        Q_VC1.append(Q_rad_-Q_teto-Q_12)
+    
+    plt.figure(figsize=(16, 9))
+    if type[1] == 'Q':
+        plt.title("Calor trocado total pela estufa pelo tempo")
+        plt.plot(d/1440, Q_VC, label="Q_VC")
+
+    match type[0]:
+        case '1':
+            plt.title("Calor trocado total pelo VC1 estufa pelo tempo")
+            if type[2] == 'Q': plt.plot(d/1440, Q_VC1, label="Q_VC1")
+            if type[3] == 'C': plt.plot(d/1440, Q_VC1_corr(di, days), label="Q_VC1_corr")
+        case '2':
+            plt.title("Calor trocado total pelo VC2 estufa pelo tempo")
+            if type[2] == 'Q': plt.plot(d/1440, days*[Q_VC2], label="Q_VC2")
+            if type[3] == 'C': plt.plot(d/1440, Q_VC2_corr(di, days), label="Q_VC2_corr")
+        case '3':
+            plt.title("Calor trocado total pelo VC3 estufa pelo tempo")
+            if type[2] == 'Q': plt.plot(d/1440, days*[Q_VC3], label="Q_VC3")
+            if type[3] == 'C': plt.plot(d/1440, Q_VC3_corr(di, days), label="Q_VC3_corr")
+        case 'm':
+            plt.title("Correção da vazão pelo tempo")
+            plt.plot(d/1440, m_nh3_corr(di, days), label="m_nh3_corr")
+            plt.ylabel("Ajuste de vazão (kg/s)")
+    
+    if type[0] != 'm': plt.ylabel("Calor trocado pelo VC (W)")
+    plt.xlabel("Tempo (dia do ano)")
+    plt.rc('legend', fontsize=12)
+    plt.legend(loc="upper left")
+    plt.show()
+    
+plot('m___',0,365)
+
+'''
+d = np.linspace(0,90*86400,43200)
+Q_  = []
+Q_aj = []
+
+T_min = 285.8
+T_max = 302
+T_ar = 283
+
+for d_ in d:
+    Q_.append((Q_rad(d_)-Q_teto-Q_solo-Q_rsolo))
+    
+    T0_ar = T_ar
+    T_ar = T0_ar + (T_max-T_min)*np.sin(np.pi*(d_-5*3600)/(14*3600))
+    
+    Q_aj.append(-m_ar*cp_ar*(T_ar-T0_ar))
+
+plt.figure(figsize=(16, 9))
+
+plt.title("Calor trocado e correção total no VC (estufa completa) pelo tempo")
+plt.plot(d/86400, Q_, label="Q_VC")
+plt.plot(d/86400, Q_aj, label="Q_aj")
+plt.ylabel("Calor trocado pelo VC (W)")
+plt.xlabel("Tempo (dia do ano)")
+plt.legend(loc="upper left")
+plt.show()'''
